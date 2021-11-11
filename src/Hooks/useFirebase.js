f0dac1e75119df3e7068e117132c8e3e2b8c162e
taseenbappi <taseen.bappi@gmail.com>
2021-11-12 03:19:29 +0600

    firebase auth setup and usefirebase hook added

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,4 +1,94 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    updateProfile
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import authInit from "../Pages/Login/Firebase/firebase.init";
+
+authInit();
+
 const useFirebase = () => {
+
+    const [user, setUser] = useState({});
+    const [authError, setAuthError] = useState('');
+    const auth = getAuth();
+
+    // user email and password register hangler
+    const registerHangler = (name, email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                setUser(user);
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                }).then(() => {
+                    // Profile updated!
+                    setUser(user);
+                }).catch((error) => {
+                    // An error occurred
+                    // ...
+                });
+                // ...
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                setAuthError(errorMessage)
+                // ..
+            });
+    }
+    // register users login hangler
+    const registerUserSignInHangler = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                setUser(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                setAuthError(errorMessage);
+            });
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                // ...
+            } else {
+                // User is signed out
+                // ...
+            }
+            return () => unsubscribe;
+        });
+    }, [])
+
+    // logout hangler
+    const logOut = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            setUser({});
+        }).catch((error) => {
+            // An error happened.
+            const errorMessage = error.message;
+            setAuthError(errorMessage);
+        });
+    }
+    return {
+        registerHangler,
+        registerUserSignInHangler,
+        logOut,
+        user,
+        authError
+    };
 
 }
 export default useFirebase;
---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDiWcUpNfXvUtD6bPCW6t4kG7Ejdsnrb14",
+    authDomain: "baby-shopper-bd.firebaseapp.com",
+    projectId: "baby-shopper-bd",
+    storageBucket: "baby-shopper-bd.appspot.com",
+    messagingSenderId: "202977010765",
+    appId: "1:202977010765:web:56ea26b3a8f23e1ccd2804"
+};
+export default firebaseConfig;
